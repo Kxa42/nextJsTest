ef16a733f8ba636b22866ead327083870060f46c
kuang_xiao <2190540495@qq.com>
2024-09-24 19:13:33 +0800

    to 12 chapter

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    //最新版本的nextjs才能够使用ppr功能，我现在用的是nextjs14，所以无法启用该功能
+    // experimental: {
+    //   ppr: 'incremental',
+    // },
+  };
 
 export default nextConfig;
